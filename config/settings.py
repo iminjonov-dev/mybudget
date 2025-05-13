618430db23d8a150a16e43bf2737cace0a8b4520
iminjonov-dev <161212692+iminjonov-dev@users.noreply.github.com>
2025-05-13 08:54:47 +0500

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -53,6 +53,7 @@ MIDDLEWARE = [
     'django.middleware.locale.LocaleMiddleware',
 
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
